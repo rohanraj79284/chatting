3fa67a9b0b1503f642bde1ec71bdc37287ff6ddf
rohanraj79284 <65460991+rohanraj79284@users.noreply.github.com>
2025-01-14 12:20:35 -0800

    Add files via upload

---
+++ chatApp/index.js
@@ -0,0 +1,54 @@
+const express=require('express');
+const http=require('http');
+const socketIo=require('socket.io');
+const path=require('path');
+const multer = require('multer');
+
+
+const app= express();
+const server=http.createServer(app);
+
+
+const io= socketIo(server);
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        return cb(null,"./uploads");
+    },
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}-${file.originalname}`);
+    }
+})
+io.on('connection',(socket)=>{
+    console.log('new connection has been established\n');
+    socket.on('user-msg',(message)=>{
+        io.emit('msg',message);
+    })
+
+})
+// const server=http.createServer(app);
+
+
+app.get("/",(req,res,next)=>{
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile("index.html",options);
+})
+
+
+app.use(express.urlencoded({extended: false}));
+
+const upload=multer({storage: storage});
+
+app.post("/uploads",upload.single("profileImage"),(req,res,next)=>{
+    console.log(req.body);
+    console.log(req.file);
+     res.redirect("/");
+})
+
+const port=3001;
+server.listen(port,()=>{
+    console.log(`server running on http://127.0.0.1:${port}`);
+})
+
